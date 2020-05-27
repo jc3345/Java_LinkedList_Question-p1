95e5c9072b08f1d2809630f7862102d48f154bca
jc3345 <65318989+jc3345@users.noreply.github.com>
2020-05-27 13:30:30 -0700

    Create singleton.java

---
+++ singleton.java
@@ -0,0 +1,21 @@
+/**
+* @author      Jiachao Chen <jc3345@cornell.edu>
+
+*<p>
+* What are some ways to implement a singleton in Java：
+* 1. Eager initialization
+* 2. Static block initialization
+* 3. Lazy Initialization
+* 4. Thread Safe Singleton
+* 5. Bill Pugh Singleton Implementation
+* 6. Enum Singleton
+* singleton common concepts:
+* Private constructor to restrict instantiation of the class from other classes.
+* Private static variable of the same class that is the only instance of the class.
+* Public static method that returns the instance of the class.
+
+* Reference: retrieved from JournalDev: https://www.journaldev.com/1377/java-singleton-design-pattern-best-practices-examples#enum-singleton
+*<p>
+
+
+ */
